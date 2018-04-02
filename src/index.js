13041f3f46e046518e07eeb41327f5579da1559f
github username <github email address>
2018-04-01 22:32:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+const meInReact =
+  React.createElement('div', { className:'me'},
+    React.createElement('h1', {}, 'An Awesome Person'),
+    React.createElement('p', {}, 'Who is learning React'),
+    React.createElement('ul', {className: 'my-interests'},
+      [
+        React.createElement('li', {}, 'Javascript'),
+        React.createElement('li', {}, 'Yoga'),
+        React.createElement('li', {}, 'Art'),
+        React.createElement('li', {}, 'Music')
+      ]
+    )
+  );
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
 );
-
-export default meInReact
-*/
